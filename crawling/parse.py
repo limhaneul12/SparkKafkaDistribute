bb32810c40d3821157701429cb21d726becd3ddc
sky81219 <=>
2023-01-19 00:52:24 +0900

    대용량 spark 분석

--- crawling/parse.py
+++ crawling/parse.py
@@ -3,7 +3,7 @@ import pathlib
 import collections
 import calendar
 import logging
-from typing import List
+from typing import List, Final
 from queue import Queue
 
 from urllib.request import urlretrieve
@@ -13,12 +13,12 @@ if not hasattr(collections, 'Callable'):
     collections.Callable = collections.abc.Callable
 
 
-PATH = f"{pathlib.Path(__file__).parent.parent}/sparkAnaliysis/data"
-CPATH = f"{os.getcwd()}/data"
+PATH: Final[str] = f"{pathlib.Path(__file__).parent.parent}/sparkAnaliysis/data"
+CPATH: Final[str] = f"{os.getcwd()}/data"
 
 
 try:
-    os.mkdir(f"{CPATH}/")
+    os.mkdir(f"{PATH}/")
 except FileExistsError:
     logging.info(f'이미 메인 파일이 존재합니다.')
 
@@ -29,41 +29,41 @@ tlc_url: str = gd().page()
 bs = BeautifulSoup(tlc_url, "html.parser")    
 
 
-def dirctory_download(element: BeautifulSoup) -> List[str]:
+def file_download(element: BeautifulSoup) -> List[str]:
     return [data["href"] for data in element.find_all("a", {"title": "High Volume For-Hire Vehicle Trip Records"})]
 
 
-def file_download(start: int, end: int) -> None:
+def folder_making(start: int, end: int, path: str) -> None:
     for j in range(start, end-1, -1):
         for i in bs.find_all("div", {"data-answer": f"faq20{j}", "class": "faq-questions collapsed"}):
             try:
                 name = i.text.replace("\n", "")
-                os.mkdir(f"{CPATH}/{name}/")
+                os.mkdir(f"{path}/{name}/")
             except (FileExistsError, ValueError):
                 continue 
 
         
-def search_injection(start: int, end: int) -> None:
+def search_injection(start: int, end: int, path: str) -> None:
     for i in range(start, end-1, -1):
         for inner in bs.find_all("div", {"class": "faq-answers", "id": f"faq20{i}"}): 
-            file_download(start=start, end=end)
-            data_struct: List[str] = dirctory_download(inner)
+            folder_making(start=start, end=end, path=path)
+            data_struct: List[str] = file_download(inner)
             q.put(data_struct)
 
 
-def download(n: int) -> None:
+def download(n: int, path: str) -> None:
     j: int = 0
     while j < n:
         logging.info(f"{j}번째 색션에 접근합니다")
         for da in q.get():
             name: str = da.split("/")[4]
             name_number: int = int(name.split("_")[2].split("-")[0])
-            file_location: str = f"{CPATH}/{name_number}/{name}"
+            file_location: str = f"{path}/{name_number}/{name}"
             logging.info(f"{file_location} 저장합니다")
             urlretrieve(da, file_location)
         j+=1     
 
 
 
-search_injection(start=22, end=20)
+search_injection(start=22, end=20, path=PATH)
 download(n=3)
