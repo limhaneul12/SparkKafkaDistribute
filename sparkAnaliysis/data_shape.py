71de8c0e75eb83fc84af8d6802243a25d5a70617
sky81219 <=>
2023-01-27 01:34:57 +0900

    대참사

--- sparkAnaliysis/data_shape.py
+++ sparkAnaliysis/data_shape.py
@@ -5,14 +5,21 @@ import pyarrow
 
 path = (f"{os.getcwd()}/data/{i}" for i in os.listdir(f"{os.getcwd()}/data"))
 
-d = 0
-for p in path: 
-    sd = os.listdir(p)
-    sd.sort()
-    for data in sd:
-        l = f"{p}/{data}"
-        f = pd.read_parquet(l)
-        d += f.shape[0]
-        print(l ,f.shape[0])
-print(f"총 데이터 개수 --> {d}")
+def test():
+    d = 0
+    for p in path: 
+        sd = os.listdir(p)
+        sd.sort()
+        for data in sd:
+            l = f"{p}/{data}"
+            f = pd.read_parquet(l)
+            d += f.shape[0]
+            print(l ,f.shape[0])
+    print(f"총 데이터 개수 --> {d}")
 
+
+
+location = "/Users/imhaneul/Documents/spark-kafka-distribute/sparkAnaliysis/data/2020/fhvhv_tripdata_2020-01.parquet"
+data = pd.read_parquet(location)
+
+print(data.columns)
